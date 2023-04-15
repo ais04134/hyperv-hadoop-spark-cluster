9d1b87e2abbb51537052320e3dc1d4a26bcbb16e
geon <ais04134@naver.com>
2023-04-15 23:08:39 +0900

    WebUI 실행 스크립트

---
+++ Shell script/Hadoop-Ecosystem/WebUI.ps1
@@ -0,0 +1,20 @@
+﻿<#
+Hadoop-Ecosystem에 대한 Web UI를 실행합니다.
+#>
+
+# Spark WEB UI 확인
+start chrome http://192.168.0.18:18080/
+
+# Yarn WEB UI 확인
+start chrome http://192.168.0.18:18080/
+
+# Hadoop WEB UI 확인(Active)
+start chrome http://192.168.0.18:18080/
+
+# Hadoop WEB UI 확인(Standby)
+start chrome http://192.168.0.18:18080/
+
+# Hadoop JobHistory
+start chrome http://192.168.0.18:18080/
+
+echo "Web UI가 모두 실행되었습니다."
