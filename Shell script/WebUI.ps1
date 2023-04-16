eb7deca719ea00052bc67226aeb59aa48f2fabca
geon <ais04134@naver.com>
2023-04-17 00:38:14 +0900

    내용 추가

--- Shell script/WebUI.ps1
+++ Shell script/WebUI.ps1
@@ -17,4 +17,22 @@ start chrome http://192.168.0.19:50070/
 # Hadoop JobHistory
 start chrome http://192.168.0.18:19888/
 
+<#
+# yarn 리소스 메니저 주소
+start chrome http://192.168.0.18:8032/
+
+# yarn 리소스 스케줄러 주소
+start chrome http://192.168.0.18:8030/
+
+# yarn 리소스 매니저 웹 응용 프로그램 주소
+start chrome http://192.168.0.18:8030/
+
+# Secondary NameNode의 HTTP 주소
+start chrome http://192.168.0.19:50080/
+
+#네임노드가JournalNode에 대한 편집 로그를 공유할 수 있도록 구성된 URI
+qjournal://nn1:8485;nn2:8485;dn1:8485/my-hadoop-cluster
+#>
+
+
 echo "Web UI가 모두 실행되었습니다."
