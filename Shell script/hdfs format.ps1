6ac6f7d3febf33d94a6f9eebea5b651934ac0fd9
geon <ais04134@naver.com>
2023-04-19 21:40:36 +0900

    전체적인 수정 hdfs 가 동작안함, settings 수정 해야될 것 같음

---
+++ Shell script/hdfs format.ps1
@@ -0,0 +1,16 @@
+﻿<#
+
+hdfs format 
+  
+#>
+
+ssh geon@192.168.0.18 hdfs namenode -format
+ssh geon@192.168.0.19 hdfs namenode -format
+
+ssh geon@192.168.0.20 hdfs datanode -format
+ssh geon@192.168.0.21 hdfs datanode -format
+ssh geon@192.168.0.22 hdfs datanode -format
+ssh geon@192.168.0.23 hdfs datanode -format
+ssh geon@192.168.0.24 hdfs datanode -format
+
+echo "hdfs format successfully."
